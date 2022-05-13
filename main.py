29f63e393e4a49ae24020789b969d303f8d4cada
Nik Afiq <nik.afiq98@ymail.com>
2022-05-13 23:42:57 +0900

    main file

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+import pyocr
+
+path='C:\\Program Files\\Tesseract-OCR\\'
+os.environ['PATH'] = os.environ['PATH'] + path
+
+pyocr.tesseract.TESSERACT_CMD = r'C:\Program Files/Tesseract-OCR\Tesseract.exe'
+tools = pyocr.get_available_tools()
+tool = tools[0]
+
+img = Image.open("C:\\Users\\nikaf\\Videos\\Captures\\umamusume 2022_04_16 0_05_05.png")
+
+builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+text = tool.image_to_string(img, lang="jpn", builder=builder)
+
+print(img)
+print(text)
